d330bcd2babe826f6b7888240d3418c9bdd0b7f6
wilson.prosdocimo <wilson.prosdocimo@malapronta.com.br>
2015-06-10 16:45:13 -0300

    Alterado modulo python utilizado para conectar com o elasticsearch pois o modulo antigo não estava funcionando com a versão 1.5.2 do elasticsearch.

--- ESzabbix.py
+++ ESzabbix.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 
-# Created by Aaron Mildenstein on 19 SEP 2012
+# Rewrite by Wilson Luiz Prosdocimo
+# Based on https://github.com/serialsito/Elasticsearch-zabbix
 
-from pyes import *
+from elasticsearch import *
 import sys
 
 # Define the fail message
 def zbx_fail():
     print "ZBX_NOTSUPPORTED"
     sys.exit(2)
-    
+
+# Define groups of keys
 searchkeys = ['query_total', 'fetch_time_in_millis', 'fetch_total', 'fetch_time', 'query_current', 'fetch_current', 'query_time_in_millis']
 getkeys = ['missing_total', 'exists_total', 'current', 'time_in_millis', 'missing_time_in_millis', 'exists_time_in_millis', 'total']
 docskeys = ['count', 'deleted']
 indexingkeys = ['delete_time_in_millis', 'index_total', 'index_current', 'delete_total', 'index_time_in_millis', 'delete_current']
 storekeys = ['size_in_bytes', 'throttle_time_in_millis']
 cachekeys = ['filter_size_in_bytes', 'field_size_in_bytes', 'field_evictions']
+jvmkeys = ['heap_used_percent']
 clusterkeys = searchkeys + getkeys + docskeys + indexingkeys + storekeys
 returnval = None
 
@@ -30,42 +33,37 @@ if len(sys.argv) < 3:
 
 # Try to establish a connection to elasticsearch
 try:
-    conn = ES('localhost:9200',timeout=25,default_indices=[''])
+    conn = Elasticsearch(request_timeout=25)
 except Exception, e:
     zbx_fail()
 
-
 if sys.argv[1] == 'cluster':
     if sys.argv[2] in clusterkeys:
-        nodestats = conn.cluster_stats()
+        nodestats = conn.nodes.stats()
         subtotal = 0
-        for nodename in nodestats['nodes']:
+        for nodename in nodestats[u'nodes']:
+            print sys.argv[2]
+            print nodename
             if sys.argv[2] in indexingkeys:
-                indexstats = nodestats['nodes'][nodename]['indices']['indexing']
+                indexstats = nodestats[u'nodes'][nodename][u'indices'][u'indexing']
             elif sys.argv[2] in storekeys:
-                indexstats = nodestats['nodes'][nodename]['indices']['store']
+                indexstats = nodestats[u'nodes'][nodename][u'indices'][u'store']
             elif sys.argv[2] in getkeys:
-                indexstats = nodestats['nodes'][nodename]['indices']['get']
+                indexstats = nodestats[u'nodes'][nodename][u'indices'][u'get']
             elif sys.argv[2] in docskeys:
-                indexstats = nodestats['nodes'][nodename]['indices']['docs']
+                indexstats = nodestats[u'nodes'][nodename][u'indices'][u'docs']
             elif sys.argv[2] in searchkeys:
-                indexstats = nodestats['nodes'][nodename]['indices']['search']
+                indexstats = nodestats[u'nodes'][nodename][u'indices'][u'search']
             try:
                 subtotal += indexstats[sys.argv[2]]
             except Exception, e:
                 pass
         returnval = subtotal
 
-
     else:
-        # Try to pull the managers object data
-        try:
-            escluster = managers.Cluster(conn)
-        except Exception, e:
-            zbx_fail()
         # Try to get a value to match the key provided
         try:
-            returnval = escluster.health()[sys.argv[2]]
+            returnval = conn.cluster.health()[unicode(sys.argv[2])]
         except Exception, e:
             zbx_fail()
         # If the key is "status" then we need to map that to an integer
@@ -83,36 +81,34 @@ if sys.argv[1] == 'cluster':
 elif sys.argv[1] == 'service':
     if sys.argv[2] == 'status':
         try:
-            conn.status()
+            conn.ping()
             returnval = 1
         except Exception, e:
             returnval = 0
 
 else: # Not clusterwide, check the next arg
 
-    nodestats = conn.cluster_stats()
-    print nodestats
-    for nodename in nodestats['nodes']:
-        if sys.argv[1] in nodestats['nodes'][nodename]['name']:
+    nodestats = conn.nodes.stats()
+    for nodename in nodestats[u'nodes']:
+        if sys.argv[1] in nodestats[u'nodes'][nodename][u'name']:
             if sys.argv[2] in indexingkeys:
-                stats = nodestats['nodes'][nodename]['indices']['indexing']
+                stats = nodestats[u'nodes'][nodename][u'indices'][u'indexing']
             elif sys.argv[2] in storekeys:
-                stats = nodestats['nodes'][nodename]['indices']['store']
+                stats = nodestats[u'nodes'][nodename][u'indices'][u'store']
             elif sys.argv[2] in getkeys:
-                stats = nodestats['nodes'][nodename]['indices']['get']
+                stats = nodestats[u'nodes'][nodename][u'indices'][u'get']
             elif sys.argv[2] in docskeys:
-                stats = nodestats['nodes'][nodename]['indices']['docs']
+                stats = nodestats[u'nodes'][nodename][u'indices'][u'docs']
             elif sys.argv[2] in searchkeys:
-                stats = nodestats['nodes'][nodename]['indices']['search']
-            elif sys.argv[2] in cachekeys:
-                stats = nodestats['nodes'][nodename]['indices']['cache']
+                stats = nodestats[u'nodes'][nodename][u'indices'][u'search']
+            elif sys.argv[2] in jvmkeys:
+                stats = nodestats[u'nodes'][nodename][u'jvm'][u'mem']
             try:
-                returnval = stats[sys.argv[2]]
+                returnval = stats[unicode(sys.argv[2])]
             except Exception, e:
                 pass
 
-
-# If we somehow did not get a value here, that's a problem.  Send back the standard 
+# If we somehow did not get a value here, that's a problem.  Send back the standard
 # ZBX_NOTSUPPORTED
 if returnval is None:
     zbx_fail()
@@ -120,4 +116,4 @@ else:
     print returnval
 
 # End
-
+nd
